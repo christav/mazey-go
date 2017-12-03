5aab64b5d101bd80f26f72cad38c525253f639c9
Chris Tavares <ctavares@microsoft.com>
2017-12-03 01:36:51 -0800

    Added command line args for width and height

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"mazey/maze"
 	"os"
 )
 
+var width = flag.Int("w", 30, "Width of the maze")
+var height = flag.Int("h", 20, "Height of the maze")
+
 func main() {
-	m := maze.MakeMaze(20, 30)
+	flag.Parse()
+	m := maze.MakeMaze(*height, *width)
 	maze.Solve(m)
 	maze.PrintMaze(m, os.Stdout)
 }
